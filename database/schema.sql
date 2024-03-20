ccc198d1f5a9d357d835917831b3ddd3e13280fb
Robert Gardner <rdgardner.dev@gmail.com>
2024-03-20 15:05:03 -0700

    Add optional column for userId, and users table.

--- database/schema.sql
+++ database/schema.sql
@@ -6,8 +6,18 @@ drop schema "public" cascade;
 
 create schema "public";
 
+create table "public"."users" (
+  "userId"         serial,
+  "username"       text not null,
+  "hashedPassword" text not null,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
+
 create table "public"."entries" (
   "entryId"    serial,
+  "userId"     integer,
   "title"      text    not null,
   "notes"      text    not null,
   "photoUrl"   text    not null,
